a3cffe8e36793859501cd0b8112c48200377dbba
POTHUMAHANTHI MANOHAR <manohar162004@gmail.com>
2024-12-10 08:38:06 +0530

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,14 +3,10 @@
 import * as React from "react"
 import Link from "next/link"
 
-import { cn } from "@/lib/utils"
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 
